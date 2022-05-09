e3e248756eed9b7f91fdfce57d2f0b3942cd9c9d
akshay2408 <akshaybabar2408@gmail.com>
2022-05-09 17:56:22 +0530

    Create model for the project

--- TeacherStudentassignmenttask/api/models.py
+++ TeacherStudentassignmenttask/api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Teacher(models.Model):
+    name = models.ManyToManyField(to='teacher', blank=True, null=True)
+
+
+class Student(models.Model):
+    name = models.ManyToManyField(to='student', blank=True, null=True)
+
+
+class TeacherStudent(models.Model):
+    teacher = models.ForeignKey(to='teacher',on_delete=models.CASCADE, blank=True, null=True)
+    Student = models.ForeignKey(to='student', on_delete=models.CASCADE, blank=True, null=True)
+    is_star = models.BooleanField(default=False, blank=True, null=True)
